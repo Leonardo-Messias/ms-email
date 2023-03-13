2404e2bc27ca4b47d1595b1577d3f5c60748a411
loeb <leonardo.barboza@gft.com>
2023-03-13 14:33:33 -0300

    First Commit

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,17 @@
+package com.ms.email.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "TB_EMAIL")
+public class EmailModel {
+
+    }
+
+
+}
