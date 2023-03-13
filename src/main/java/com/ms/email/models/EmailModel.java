1a3619da12210e8e89636ffe875144b001cfc81b
loeb <leonardo.barboza@gft.com>
2023-03-13 14:42:11 -0300

    creating email entity attributes

--- src/main/java/com/ms/email/models/EmailModel.java
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -3,6 +3,7 @@ package com.ms.email.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -10,7 +11,22 @@ import java.time.LocalDateTime;
 @Data
 @Table(name = "TB_EMAIL")
 public class EmailModel {
+    public class EmailModel implements Serializable {
+        @Serial
+        private static final long serialVersionUID = 1L;
 
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long emailId;
+        private String ownerRef;
+        private String emailFrom;
+        private String emailTo;
+        private String subject;
+        @Column(columnDefinition = "TEXT") // para que o banco aceite mais de 255 caracteres
+        private String text;
+
+        private LocalDateTime sendDateEmail;
+        private StatusEmail statusEmail;
     }
 
 
